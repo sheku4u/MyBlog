23c989de867a578aba9ab825caff0138e6b723d4
Abhishek Bansal <Abhishekbansal27087@gmail.com>
2024-03-20 21:31:20 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Protected from "./AuthLayout"
+import Button from './Button'
+import Input from './Input'
+import Login from './Login'
+import Logo from './Logo';
+import PostCard from './PostCard'
+import RTE from './RTE'
+import Select from './Select'
+import Signup from './Signup'
+import Container from '../components/container/Container'
+import PostForm from './post-form/PostForm'
+import LogoutBtn from "./header/LogoutBtn";
+import AuthLayout from './AuthLayout'
+export {Header,
+    LogoutBtn,Footer, Protected,Button,Input,Login,Logo,PostCard,RTE,Select,Signup,PostForm,Container,
+AuthLayout}
